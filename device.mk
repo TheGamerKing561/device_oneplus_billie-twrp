c75c8885025f7384469293926aca168ba80e239e
Eyad Tamer <eyadtamer2121@gmail.com>
2026-07-03 14:47:59 +0300

    Remove more libs that are already built from source

--- device.mk
+++ device.mk
@@ -49,11 +49,13 @@ PRODUCT_PACKAGES += \
 # Extra libraries that are needed for decryption
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    vendor.display.config@1.0
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
 PRODUCT_PACKAGES += \
     android.system.keystore2
