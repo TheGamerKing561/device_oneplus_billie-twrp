1a368fbe80711e8b9352d8dcbf60462b72dccb79
Eyad Tamer <eyadtamer2121@gmail.com>
2026-07-05 23:02:37 +0300

    Use QTI bootctrl hal v1.2

--- device.mk
+++ device.mk
@@ -35,8 +35,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl-qti.recovery \
-    bootctrl.lito.recovery \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service \
+    bootctrl.lito \
+    bootctrl.lito.recovery
 
 # qcom decryption
 PRODUCT_PACKAGES += \
