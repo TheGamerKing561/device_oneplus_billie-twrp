c64b3c19697477496cd547c1657d5b15564b7abc
Eyad Tamer <eyadtamer2121@gmail.com>
2026-07-08 23:22:25 +0300

    Build android.hidl.allocator@1.0 and android.hidl.memory@1.0

--- device.mk
+++ device.mk
@@ -54,6 +54,8 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
     libdmabufheap \
     libhidlmemory \
+    android.hidl.allocator@1.0 \
+    android.hidl.memory@1.0 \
     vendor.display.config@1.0 \
     vendor.display.config@2.0
 
@@ -61,6 +63,8 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
