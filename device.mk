23908a5ee4bc0e81cbb85cbd0ef744a0fff4b6c9
Eyad Tamer <eyadtamer2121@gmail.com>
2026-07-08 14:26:33 +0300

    Build more needed libs

--- device.mk
+++ device.mk
@@ -52,11 +52,15 @@ PRODUCT_PACKAGES += \
 # Extra libraries that are needed for decryption
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
+    libdmabufheap \
+    libhidlmemory \
     vendor.display.config@1.0 \
     vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
